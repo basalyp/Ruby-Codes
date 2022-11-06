d223d7ee797789777f0a599745ec7240375ec332
basalyp <117032642+basalyp@users.noreply.github.com>
2022-11-06 17:13:45 -0500

    Add files via upload

---
+++ Calculator/Calculator.rb
@@ -0,0 +1,7 @@
+puts("Please enter your first number: ")
+num1= gets.chomp()
+puts ("Please enter your second number: ")
+num2= gets.chomp()
+sum = num1.to_f+num2.to_f
+
+puts ("The sum is: " + sum.to_s )
